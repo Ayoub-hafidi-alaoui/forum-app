92fd1755467947b0d90503138f680c9467c6642d
Ayoub <ayoub.hafidi.alaoui@hotmail.fr>
2021-10-31 00:39:41 +0200

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import styled from 'styled-components'
 function Header() {
     return (
        <Wrapper>
-           <ContentWrapper>
+           <ContentWrapper> 
+               <LogoWrapper>
+                   <Logo src="https://www.ptvgroup.com/fileadmin/_processed_/5/e/csm_Header-Developer-Zone_810d67bc5f.jpg" alt="developer"/>
+                   <LogoTitle>Developer</LogoTitle>
+               </LogoWrapper>
            </ContentWrapper>
        </Wrapper>
     )
@@ -22,4 +26,25 @@ const Wrapper=styled.div`
     z-index: 1;
     box-shadow : 0px 2px 4px rgba(0,0,0,0.1);
 `
-const ContentWrapper=styled.div``
+const ContentWrapper=styled.div`
+    width:1270px;
+    height: 90%;
+    display:flex;
+    align-items: center;
+`
+const LogoWrapper=styled.div`
+    display: flex;
+    height: 60%;
+    width: 20%;
+    justify-content: space-between;
+`
+const Logo=styled.img`
+    height: 100%;
+    width: auto;
+
+`
+const LogoTitle=styled.div`
+    font-family: 'bebas-neue-pro';
+    font-size: 34px;
+    font-weight: 600;
+`
